ba2403815bf7b9406b86683bed9d7145ec5c9471
heavyrisem <heavyrisem@gmail.com>
2024-02-26 19:09:38 +0900

    feat: backend setup

---
+++ apps/backend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ["withme"],
+  rules: {
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "n/no-path-concat": "off",
+    "@typescript-eslint/ban-ts-comment": "warn"
+  }
+};
+
